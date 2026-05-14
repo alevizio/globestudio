eb170be158fbe5227d28638a0cb59ef16ef3c536
Alejandro <viziomas@gmail.com>
2026-05-14 16:31:22 -0700

    Keep local Vite base at root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/worlddots/",
+  base: process.env.GITHUB_ACTIONS ? "/worlddots/" : "/",
   plugins: [react()],
 });
